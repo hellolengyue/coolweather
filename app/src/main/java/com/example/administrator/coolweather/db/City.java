7d58db24f81019f73ab45d9f0e0e609b3ccc4a54
hel <1135090898@qq.com>
2018-06-26 18:28:34 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/administrator/coolweather/db/City.java
@@ -0,0 +1,49 @@
+package com.example.administrator.coolweather.db;
+
+import org.litepal.crud.LitePalSupport;
+
+/**
+ * @author hel
+ * @date 2018/6/26
+ * 文件 CoolWeather
+ * 描述
+ */
+
+public class City extends LitePalSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName == null ? "" : cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
---
+++ app/src/main/java/com/example/administrator/coolweather/db/County.java
@@ -0,0 +1,49 @@
+package com.example.administrator.coolweather.db;
+
+import org.litepal.crud.LitePalSupport;
+
+/**
+ * @author hel
+ * @date 2018/6/26
+ * 文件 CoolWeather
+ * 描述
+ */
+
+public class County extends LitePalSupport {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName == null ? "" : countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId == null ? "" : weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
